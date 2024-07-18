9dbd402b3ca8f05888e24f92f9a68f3680d9d80e
vilkt <vilkt@gmail.com>
2024-07-18 23:37:33 +0200

    improve templates

--- command/version.go
+++ command/version.go
@@ -5,7 +5,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "v0.1.3"
+const version = "v0.1.4"
 
 func Version(cmd *cobra.Command, args []string) {
 	fmt.Println("My CLI tool " + version)
