433f776f04b5d2936b24a3e5b7108be545afd802
victorivanov <prgmac@mail.ru>
2024-07-27 17:33:52 +0200

    version up

--- command/version.go
+++ command/version.go
@@ -5,7 +5,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "v0.1.8"
+const version = "v0.1.9"
 
 func Version(cmd *cobra.Command, args []string) {
 	fmt.Println("My CLI tool " + version)
