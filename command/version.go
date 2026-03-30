bbbf006d4bd55b92a45a59259bbd7cbde388484d
victorivanov <prgmac@mail.ru>
2026-03-31 00:33:39 +0200

    bump version to v0.1.20

--- command/version.go
+++ command/version.go
@@ -2,10 +2,11 @@ package command
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
-const version = "v0.1.19"
+const version = "v0.1.20"
 
 func Version(cmd *cobra.Command, args []string) {
 	fmt.Println("My CLI tool " + version)
